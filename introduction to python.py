22d3d85d361290f29ba5f320d796787d17226d99
Samir Omer <samiromer2@yahoo.com>
2026-09-10 09:27:52 -0400

    numpy

--- introduction to python.py
+++ introduction to python.py
@@ -108,3 +108,14 @@ print("Area: " + str(A))
 #----------
 
 #Numpy
+
+# Import the numpy package as np
+import numpy as np
+
+baseball = [180, 215, 210, 210, 188, 176, 209, 200]
+
+# Create a numpy array from baseball: np_baseball
+np_baseball = np.array(baseball)
+
+# Print out type of np_baseball
+print(type(np_baseball))
