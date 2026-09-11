988ece47bb3b2fab14ff89a6592670e4197d19e2
Samir Omer <samiromer2@yahoo.com>
2026-09-10 23:14:34 -0400

    one step

--- introduction to python.py
+++ introduction to python.py
@@ -119,3 +119,19 @@ np_baseball = np.array(baseball)
 
 # Print out type of np_baseball
 print(type(np_baseball))
+
+
+# Import numpy
+#import numpy as np
+height_in = [180, 215, 210, 210, 188, 176, 209, 200]
+# Create a numpy array from height_in: np_height_in
+np_height_in = np.array(height_in)
+
+# Print out np_height_in
+print(np_height_in)
+
+# Convert np_height_in to m: np_height_m
+np_height_m = np_height_in * 0.0254
+
+# Print np_height_m
+print(np_height_m)
