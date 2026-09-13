3d9db99a834a3c71de34033c5ac59d8fb9833249
Samir Omer <samiromer2@yahoo.com>
2026-09-13 03:22:57 -0400

    in the neck of time trying my best

--- introduction to python.py
+++ introduction to python.py
@@ -137,7 +137,7 @@ np_height_m = np_height_in * 0.0254
 print(np_height_m)
 
 
-import numpy as np
+##import numpy as np
 
 np_weight_lb = np.array(weight_lb)
 np_height_in = np.array(height_in)
@@ -147,3 +147,86 @@ print(np_weight_lb[50])
 
 # Print out sub-array of np_height_in: index 100 up to and including index 110
 print(np_height_in[100:111])
+
+
+##------------------
+##import numpy as np
+
+baseball = [[180, 78.4],
+            [215, 102.7],
+            [210, 98.5],
+            [188, 75.2]]
+
+# Create a 2D numpy array from baseball: np_baseball
+np_baseball = np.array(baseball)
+
+# Print out the type of np_baseball
+print(type(np_baseball))
+
+# Print out the shape of np_baseball
+print(np_baseball.shape)
+
+##############
+#import numpy as np
+
+# Create a 2D numpy array from baseball: np_baseball
+np_baseball = np.array(baseball)
+
+# Print out the shape of np_baseball
+print(np_baseball.shape)
+
+##############
+
+import numpy as np
+
+np_baseball = np.array(baseball)
+
+# Print out the 50th row of np_baseball
+print(np_baseball[49,:])
+
+# Select the entire second column of np_baseball: np_weight_lb
+np_weight_lb = np_baseball[:,1]
+
+# Print out height of 124th player
+print(np_baseball[123,0])
+
+import numpy as np
+
+np_baseball = np.array(baseball)
+
+# Print out addition of np_baseball and updated
+print(np_baseball + updated )
+
+# Create numpy array: conversion
+conversion = np.array([0.0254,0.453592,1])
+
+# Print out product of np_baseball and conversion
+print(np_baseball*conversion)
+#------------
+import numpy as np
+
+# Create np_height_in from np_baseball
+np_height_in = np_baseball[:,0]
+
+# Print out the mean of np_height_in
+print(np_height_in.mean())
+
+# Print out the median of np_height_in
+print(np.median(np_height_in))
+
+##-----------------
+
+avg = np.mean(np_baseball[:,0])
+print("Average: " + str(avg))
+
+# Print median height
+med = np.median(np_baseball)
+print("Median: " + str(med))
+
+# Print out the standard deviation on height
+stddev = np.std(np_baseball[:,0])
+print("Standard Deviation: " + str(stddev))
+
+# Print out correlation between first and second column
+corr = np.corrcoef(np_baseball[:,0],np_baseball[:,1])
+print("Correlation: " + str(corr))
