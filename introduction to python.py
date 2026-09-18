2e1e90c0342b1a5e27b2098f655127f363bbdd4c
Samir Omer <samiromer2@yahoo.com>
2026-09-18 00:55:02 -0400

    some notes

--- introduction to python.py
+++ introduction to python.py
@@ -205,7 +205,7 @@ print(np_baseball*conversion)
 #------------
 import numpy as np
 
-# Create np_height_in from np_baseball
+# Create np_height_in from np_baseball 
 np_height_in = np_baseball[:,0]
 
 # Print out the mean of np_height_in
@@ -230,3 +230,57 @@ print("Standard Deviation: " + str(stddev))
 # Print out correlation between first and second column
 corr = np.corrcoef(np_baseball[:,0],np_baseball[:,1])
 print("Correlation: " + str(corr))
+
+
+# NumPy subsetting 
+
+# bmi 
+# bmi[1]
+# bmi > 23 true and false values
+# bmi[bmi > 23] the data
+#np_2d = np.array([[1,2,3],[4,5,6],[7,8,9]])
+#np_2d.shape
+
+# Your recent learnings
+
+# When you left 16 hours ago, you worked on NumPy, chapter 4 of the course Introduction to Python. Here is what you covered in your last lesson:
+
+# You learned about the fundamentals of NumPy, a powerful Python package for data science, and how it can efficiently handle array operations. Here are the key points you covered:
+
+#     Introduction to NumPy: You discovered that NumPy arrays are an alternative to Python lists, allowing for fast, element-wise calculations.
+#     Creating NumPy Arrays: You used np.array() to convert Python lists into NumPy arrays.
+
+#     import numpy as np
+#     np_baseball = np.array(baseball)
+
+#     Element-wise Operations: You learned that operations on NumPy arrays are performed element-wise, unlike Python lists.
+
+#     np_height_m = np_height_in * 0.0254
+
+#     Type Consistency: NumPy arrays require all elements to be of the same type, converting different types to a common one if necessary.
+#     Array Arithmetic: Arithmetic operations like +, -, *, and / behave differently with NumPy arrays compared to Python lists.
+
+#     result = np.array([True, 1, 2]) + np.array([3, 4, False])
+
+#     Subsetting: You practiced subsetting NumPy arrays using square brackets and boolean arrays.
+
+#     print(np_height_in[100:111])
+
+# These concepts form the foundation for efficient data manipulation and analysis using NumPy.
+
+# The goal of the next lesson is to advance your skills in analyzing and manipulating data using NumPy's 2D arrays for more complex data analysis tasks.
+
+#numpy.ndarry
+
+# np_2d[0][2]
+# np_2d[0,2]
+
+#Generate data 
+
+height = np.round(np.random.normal(1.75, 0.20, 5000),2)
+
+weight = np.round(np.random.normal(70, 10, 5000),2)
+
+npclass = np.column_stack((height,weight))
+
+print(npclass)
